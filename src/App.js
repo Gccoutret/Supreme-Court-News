58604c4b9ba7d9e35698579caff862125d761b31
GCout <Gccoutret@gmail.com>
2020-07-06 19:17:34 -0500

    seccond commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import rbg from './rbg.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Supreme Court News
+        </h1>
+        <img src={rbg} 
+        className="Rbg-photo"
+        alt="Ruth" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        What have Ruth and the gang been up to lately?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
